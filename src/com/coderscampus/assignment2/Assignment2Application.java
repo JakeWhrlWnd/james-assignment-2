17f59758395b2bef0ea018e317930dc655f3f6d9
James <justjamescreative@outlook.com>
2025-01-05 02:19:53 -0500

    Updated Assignment 2
    Generated a random number. Created a while loop to get input from the player.

--- src/com/coderscampus/assignment2/Assignment2Application.java
+++ src/com/coderscampus/assignment2/Assignment2Application.java
@@ -1,10 +1,38 @@
 package com.coderscampus.assignment2;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Assignment2Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+//		Generate a random number
+		Random random = new Random();
+		int randomNumber = random.nextInt(1, 100);
+		
+//		Test to make sure the randomNumber that is generated is inclusive
+//		for (int i = 0; i < 100; i++) {
+//			System.out.println(randomNumber.nextInt(1, 100));
+//		}
+		
+//		Number of guesses
+		int guesses = 0;
 
+//		Collect the guesses from the user
+		while (guesses < 5) {
+			System.out.println("Pick a number between 1 and 100");
+		
+			Scanner scanner = new Scanner(System.in);
+			String userGuess = scanner.next();
+			int convertedGuess = Integer.parseInt(userGuess);
+
+			
+			guesses++;
+		}
+		
+		System.out.println("The number to guess was: " + randomNumber);
+		
 	}
 
 }
