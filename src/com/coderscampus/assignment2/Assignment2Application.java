acd0f9a070b93ec373bff6139a37ae3ac646475f
James <justjamescreative@outlook.com>
2025-01-07 13:57:51 -0500

    Updated Assignment2Application
    Based on suggestions from the code review. I removed the comments. Removed some line breaks. Created a constant for the max amount of guesses. Moved the "Pick a number..." statement to cut down on redundancy. Changed the arrangement of the logic in the while loop. Removed the final else statement and closed the scanner.

--- src/com/coderscampus/assignment2/Assignment2Application.java
+++ src/com/coderscampus/assignment2/Assignment2Application.java
@@ -7,54 +7,39 @@ public class Assignment2Application {
 
 	public static void main(String[] args) {
 		
-//		Generate a random number
 		Random random = new Random();
 		int randomNumber = random.nextInt(100) + 1;
-		
-//		Scanner instance for user guess
 		Scanner scanner = new Scanner(System.in);
-		
-//		Number of guesses
 		int guesses = 0;
-
-//		Collect the guesses from the user
-		while (guesses < 5) {
-			System.out.println("Pick a number between 1 and 100");
-			
-//			Moved the scanner instance out, changed from a String conversion to int input
-//			String userGuess = scanner.next();
-//			int convertedGuess = Integer.parseInt(userGuess);
+		final int MAX_NUMBER_OF_GUESSES = 5;
+		System.out.println("Pick a number between 1 and 100");
+		
+		while (guesses < MAX_NUMBER_OF_GUESSES) {
 			int userGuess = Integer.parseInt(scanner.next());
+			
+			if (userGuess == randomNumber) {
+				System.out.println("You win!");
+				break;
+			}
 			
 			if (userGuess < 1 || userGuess > 100) {
 				System.out.println("Your guess is not between 1 and 100, please try again.");
-			} else if (userGuess < randomNumber) {
-				System.out.println("Please, pick a higher number");
-				guesses++;
-			} else if (userGuess > randomNumber) {
-				System.out.println("Please, pick a lower number");
-				guesses++;
-			} else {
-				System.out.println("You win!");
-				break; // Break statement to end the loop when the random number is guessed
+				continue;
 			}
 			
-//			Moved outside of the loop
-//			if (guesses == 5 && userGuess != randomNumber) {
-//				System.out.println("You lose!");
-//			} else if (userGuess == randomNumber) {
-//				System.out.println("You win!");
-//			}
-
+			if (userGuess < randomNumber) {
+				System.out.println("Please, pick a higher number");
+			} else {
+				System.out.println("Please, pick a lower number");
+			}
+			guesses++;
 		}
 		
-		if (guesses == 5) {
-			System.out.println("You lose!\n"
-					+ "The number to guess was: " + randomNumber);
-		} else {
-			System.out.println("The number to guess was: " + randomNumber);
+		if (guesses == MAX_NUMBER_OF_GUESSES) {
+			System.out.println("You lose!\n" + "The number to guess was: " + randomNumber);
 		}
 		
+		scanner.close();
 	}
 
 }
